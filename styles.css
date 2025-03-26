73c3bee164d45e3b5e24dc21c0d444727cf8ddda
vicky singh <jhrneujeh@gmail.com>
2025-03-26 20:08:33 +0530

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,18 @@
+body {
+    background-color: #569;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    overflow: hidden;
+}
+
+img {
+    width: 300px;
+    transform: translateX(-100vw);
+    animation: slide-in 1.5s forwards;
+}
+
+@keyframes slide-in {
+    100% { transform: translateX(0); }
+}
